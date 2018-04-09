b12b080a560ef9901bbb0d206e845df794ff355b
Michael Kenney <mkenney@webbedlam.com>
2018-04-08 22:25:17 -0600

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,16 @@
-FROM golang:1.10-alpine
+FROM golang:1.10-alpine AS build
+
+ENV DEFAULT_SERVICE=kubernetes \
+    DEV=true \
+    PORT=80 \
+    SECUREPORT=443 \
+    TIMEOUT=10
+
+COPY ./pkg /go/src/github.com/mkenney/k8s-proxy/pkg
+WORKDIR /go/src/github.com/mkenney/k8s-proxy/pkg
+RUN go build -o /go/bin/k8s-proxy
+
+FROM alpine
 
 LABEL org.label-schema.schema-version = 1.0
 LABEL org.label-schema.vendor = mkenney@webbedlam.com
@@ -7,17 +19,11 @@ LABEL org.label-schema.description = "This service provides HTTP proxy functiona
 LABEL org.label-schema.name = "k8s Proxy"
 LABEL org.label-schema.url = https://github.com/mkenney/k8s-proxy
 
-ENV DEFAULT_SERVICE=kubernetes \
-    DEV=true \
-    PORT=80 \
-    SECUREPORT=443 \
-    TIMEOUT=10
-
-COPY . /go/src/github.com/mkenney/k8s-proxy
-WORKDIR /go/src/github.com/mkenney/k8s-proxy/pkg
-RUN go build -o /go/bin/k8s-proxy
-
 EXPOSE 80
 EXPOSE 443
+WORKDIR /
+COPY --from=build /go/bin/k8s-proxy /k8s-proxy
+COPY ./server.crt /go/src/github.com/mkenney/k8s-proxy/server.crt
+COPY ./server.key /go/src/github.com/mkenney/k8s-proxy/server.key
 
-ENTRYPOINT ["/go/bin/k8s-proxy"]
+ENTRYPOINT ["/k8s-proxy"]
--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "
-$0: Start services
+Starting the k8s-proxy service.
 
-This script will start k8s-proxy service.
+The `k8s-proxy` service should serve all traffic on a specified port.
+Ports are not yet configurable via this script but can be set in
+\`k8s-proxy.yml\`. You must set both the exposed ports in the deployment
+and service as well as the PORT and SECUREPORT environment variables in
+the deployment.
 
-The `k8s-proxy` service should serve all traffic on port 80 (working on
-443...). Ports are not yet configurable. It will route based on the
-domain being requested. For example, http://service1.somehost will route
-the request to the TCP port exposed by `service1` (port 81), and
-http://service2.somehost will route to `service2` on port 82.
+THe proxy will route based on the domain being requested. For example,
+http://service1.somehost will route the request to the TCP port exposed
+by \`service1\` and http://service2.somehost will route to \`service2\`
+using the internal \`kube-dns\` hostname.
 
 Not for production use.
 "
@@ -17,12 +20,14 @@ kubectl delete deploy k8s-proxy
 kubectl delete service k8s-proxy
 kubectl apply -f k8s-proxy.yml
 
-pod=$(kubectl get po | grep k8s-proxy | grep -i running | awk '{print $1}')
+pod=
 while [ ! -n "$pod" ]; do
     printf "."
     pod=$(kubectl get po | grep k8s-proxy | grep -i running | awk '{print $1}')
 done
 printf "\n"
 
-kubectl get po
+echo "Service started:"
+echo "$(kubectl get service | egrep '(k8s-proxy)|(NAME)')"
+echo
 kubectl logs -f $pod
