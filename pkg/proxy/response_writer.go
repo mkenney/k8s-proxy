59c2e4d95e116b9e864672c4cb9cc491af186858
Michael Kenney <michael.kenney@returnpath.com>
2018-04-18 22:15:07 -0600

    revert writer

--- pkg/proxy/response_writer.go
+++ pkg/proxy/response_writer.go
@@ -5,38 +5,39 @@ import (
 )
 
 /*
-ResponseWriter wraps http.ResponseWriter to provide access to the status code.
+ResponseWriter wraps http.ResponseWriter instances to add a Status
+method.
 */
 type ResponseWriter struct {
-	status int
 	writer http.ResponseWriter
+	status int
 }
 
 /*
-Status returns the current HTTP status code.
+Status returns the current response status code.
 */
 func (w *ResponseWriter) Status() int {
 	return w.status
 }
 
 /*
-Header implements http.ResponseWriter.
+Header implements http.ResponseWriter
 */
 func (w *ResponseWriter) Header() http.Header {
 	return w.writer.Header()
 }
 
 /*
-Write implements http.ResponseWriter.
+Write implements http.ResponseWriter
 */
 func (w *ResponseWriter) Write(data []byte) (int, error) {
 	return w.writer.Write(data)
 }
 
 /*
-WriteHeader implements http.ResponseWriter.
+WriteHeader implements http.ResponseWriter
 */
-func (w *ResponseWriter) WriteHeader(statusCode int) {
-	w.status = statusCode
-	w.writer.WriteHeader(statusCode)
+func (w *ResponseWriter) WriteHeader(code int) {
+	w.status = code
+	w.writer.WriteHeader(code)
 }
