74542c18539b821cf970b505b9253f0ee0a71979
Michael Kenney <michael.kenney@returnpath.com>
2018-04-29 00:32:52 -0600

    context highlighting

--- test/start-dev.sh
+++ test/start-dev.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 IMAGE=mkenney/k8s-proxy:latest
 DEPLOYMENT=k8s-proxy
@@ -9,16 +9,17 @@ if [ "" = "$k8s_namespace" ]; then
     k8s_namespace="default"
 fi
 
+HIGHLIGHT=$'\033[38;5;172m'
+NORMAL=$'\033[0m'
 printf "
 This script will start the kubernetes proxy service using the \`kubectl apply\`
 command. Make sure you are configured for the correct environment.
 
-Current context:   $k8s_context
-Current namespace: $k8s_namespace
+Current context:   ${HIGHLIGHT}${k8s_context}${NORMAL}
+Current namespace: ${HIGHLIGHT}${k8s_namespace}${NORMAL}
 
 "
 read -p "Do you want to continue? [y/N]: " EXECUTE
-
 if [ "y" != "$EXECUTE" ] && [ "Y" != "$EXECUTE" ]; then
     exit 0
 fi
