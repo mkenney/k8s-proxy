11e0d5a10823a3e66273db52765496950889de10
Michael Kenney <michael.kenney@returnpath.com>
2018-05-18 00:06:59 -0600

    updates

--- .travis.entrypoint.sh
+++ .travis.entrypoint.sh
@@ -8,11 +8,8 @@ apk update && apk add git
 go get -v github.com/golang/lint/golint
 [ "0" = "$?" ] || exit 1
 
-go get -u github.com/golang/dep/cmd/dep
-[ "0" = "$?" ] || exit 2
-
 cd /go/src/github.com/mkenney/k8s-proxy/pkg
-[ "0" = "$?" ] || exit 3
+[ "0" = "$?" ] || exit 2
 
 for dir in $(go list ./... | grep -v vendor); do
     go test -timeout 20s -coverprofile=profile.out $dir
@@ -32,7 +29,7 @@ for dir in $(go list ./... | grep -v vendor); do
     result=$(golint $dir)
     if [ "" != "$result" ]; then
         echo $result
-        exit_code=5
+        exit_code=4
     fi
 done
 
