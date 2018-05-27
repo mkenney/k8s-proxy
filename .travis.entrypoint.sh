f53579edcf58d34d8c70a933b2b74c31be3fd7a6
Michael Kenney <michael.kenney@returnpath.com>
2018-05-27 15:05:41 -0600

    test cleanup

--- .travis.entrypoint.sh
+++ .travis.entrypoint.sh
@@ -2,29 +2,33 @@
 set -e
 echo "" > coverage.txt
 
+SRC=/go/src/github.com/mkenney/k8s-proxy
+
 # the alpine image doesn't come with git
 apk update && apk add git
 
 go get -v github.com/golang/lint/golint
 [ "0" = "$?" ] || exit 1
 
-cd /go/src/github.com/mkenney/k8s-proxy/pkg
+cd $SRC
 [ "0" = "$?" ] || exit 2
 
-for dir in $(go list ./... | grep -v vendor); do
-    go test -timeout 20s -coverprofile=profile.out $dir
+rm -f $SRC/coverage.txt
+for dir in $(go list ./...); do
+    echo "go test -timeout 20s -coverprofile=$SRC/profile.out $dir"
+    go test -timeout 20s -coverprofile=$SRC/profile.out $dir
     exit_code=$?
     if [ "0" != "$exit_code" ]; then
         exit $exit_code
     fi
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
+    if [ -f $SRC/profile.out ]; then
+        cat $SRC/profile.out >> $SRC/coverage.txt
+        rm $SRC/profile.out
     fi
 done
 
 exit_code=0
-for dir in $(go list ./... | grep -v vendor); do
+for dir in $(go list ./...); do
     echo "golint $dir"
     result=$(golint $dir)
     if [ "" != "$result" ]; then
