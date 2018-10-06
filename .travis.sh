19ee11461aae699ef191f857584de25ada42fbc6
Michael Kenney <mkenney@webbedlam.com>
2018-10-05 21:59:12 -0600

    build updates

--- .travis.sh
+++ .travis.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 set -e
 
+export GO111MODULE=on
+export GOFLAGS=-mod=vendor
+
 WORKDIR=$(pwd)
 exit_code=0
 
-go get -v github.com/golang/lint/golint
-[ "0" = "$?" ] || exit 1
-
-env GO111MODULE=on go build  ./...
+echo "go build ./..."
+go build  ./...
+if [ "0" != "$?" ]; then
+    exit 10
+fi
 
 for dir in $(go list ./... | grep -v vendor); do
     echo "golint $dir"
     result=$(GO111MODULE=on golint $dir)
     if [ "" != "$result" ]; then
         echo $result
-        exit_code=2
-    fi
-    if [ "0" != "$exit_code" ]; then
-        exit $exit_code
+        exit 20
     fi
 done
 
 rm -f coverage.txt
 for dir in $(go list ./... | grep -v vendor); do
-    GO111MODULE=on go test -mod=vendor -timeout=20s -coverprofile=profile.out $dir
+    go test -mod=vendor -timeout=20s -coverprofile=profile.out $dir
     exit_code=$?
     if [ "0" != "$exit_code" ]; then
-        exit 3
+        exit 30
     fi
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
